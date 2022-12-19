8a471944abec7efc01031e6230ca6fc25ac70b4d
zainabnoori <zainab.noori@and.digital>
2022-12-19 15:46:44 +0000

    Fetch and display contentful data and omit contentful secrets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import useContentful from './useContentful.js';
+import { useEffect, useState } from 'react';
+import FlowerCard from './flowerCard';
 
 function App() {
+  const { getFlowers } = useContentful();
+
+  const [flowers, setflowers] = useState([]);
+
+  useEffect(() => {
+    getFlowers().then((response) => setflowers(response))
+
+  })
   return (
     <div className="App">
       <h1>Flowers</h1>
+      {flowers.map((flower, index) => (
+        <FlowerCard key={index} flower={flower} />
+      ))}
     </div>
   );
 }
---
+++ src/flowerCard.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+const FlowerCard = ({ flower }) => {
+    return (
+        <Wrapper background={flower.photo.file.url}>
+            <TextContainer>
+                <Title>{flower.name}</Title>
+            </TextContainer>
+        </Wrapper>
+    );
+};
+
+export default FlowerCard;
+
+const Wrapper = styled.div`
+  display: grid;
+  align-items: flex-end;
+  width: 300px;
+  height: 300px;
+  margin-left: 300px;
+  margin-top: 50px;
+  text-align: center;
+  border-radius: 20px;
+  box-shadow: 0px 20px 40px rgba(52, 53, 99, 0.2),
+    0px 1px 3px rgba(0, 0, 0, 0.05);
+  background: ${(props) =>
+        props.background && `url(${props.background}) center no-repeat`};
+`;
+
+const TextContainer = styled.div`
+  cursor: pointer;
+  display: grid;
+  background: rgba(255, 255, 255, 0.3);
+  padding: 20px;
+  gap: 10px;
+
+  :hover {
+    height: fit-content;
+    width: auto;
+
+    p:last-child {
+      visibility: visible;
+      display: block;
+    }
+  }
+`;
+
+const Title = styled.p`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  margin: 0;
+  font-weight: bold;
+  color: #000000;
+  margin: 0px;
+`;
