1e8d6c3dde08ad62479f0b95955dafda85a94ac9
zainabnoori <zainab.noori@and.digital>
2023-01-04 15:45:46 +0000

    Add the post request to send the data to the mock api

--- src/ApiFetch.js
+++ src/ApiFetch.js
@@ -1,20 +1,69 @@
 import axios from "axios";
-
-//const BASE_URL = "https://jsonplaceholder.typicode.com";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
 function ApiFetch() {
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
 
-    // try {
-    //     return await fetch('https://jsonplaceholder.typicode.com/posts')
-    // } catch (error) {
-    //     console.log(error);
-    // }
+    const handleChange = (e) => {
+        setName(e.target.value);
+    };
 
+    const handleAgeChange = (e) => {
+        setAge(e.target.value);
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let res = await fetch("https://63b58eee0f49ecf508a90829.mockapi.io/names", {
+                method: "POST",
+                body: JSON.stringify({
+                    name: name,
+                    age: age,
+                }),
+            });
+            console.log("response", await res.json());
+            let resJson = await res.json();
+            if (res.status === 200) {
+                setName("");
+                setAge("");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
 
     return (
         <div>
-            <h1>Api Fetch Data</h1>
-        </div>
+            <h1>Api Fetch and Post Data</h1>
+
+            <form onSubmit={(e) => { handleSubmit(e) }}>
+                <label >
+                    Name:
+        </label><br />
+                <input
+                    type="text"
+                    value={name}
+                    required
+                    onChange={(e) => { handleChange(e) }}
+                /><br />
+                <label >
+                    Age:
+        </label><br />
+                <input
+                    type="text"
+                    value={age}
+                    required
+                    onChange={(e) => { handleAgeChange(e) }}
+                /><br />
+                <input
+                    type="submit"
+                    value="Submit"
+                />
+            </form>
+        </div >
     )
 }
 export default ApiFetch;
